aeb838b15503223bb7d547f7d2fd126bcc116312
争逐 <zhengzhujituan@gmail.com>
2020-08-10 17:01:11 +0800

    updata

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['cookie'] = require('..\\middleware\\cookie.js')
-middleware['cookie'] = middleware['cookie'].default || middleware['cookie']
-
-middleware['visits'] = require('..\\middleware\\visits.js')
-middleware['visits'] = middleware['visits'].default || middleware['visits']
-
-export default middleware
